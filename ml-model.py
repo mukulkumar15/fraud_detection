5e452940682433952f8d2e823826a67f92dc8e9a
mukulkumar15 <mukulbit1581@gmail.com>
2021-12-11 10:14:33 +0530

    added 3 new models

--- ml-model.py
+++ ml-model.py
@@ -1,1 +1,11 @@
+
 print("new ml model code")
+
+def model1():
+    print("adding code for model1") 
+
+def model2():
+    print("adding code for model2") 
+
+def model3():
+    print("adding code for model1") 
